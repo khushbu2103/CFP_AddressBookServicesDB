7b993ec7543e2a7a20b19b77185a611a430972e0
khushbu <khushbusoni475@gmail.com>
2024-01-08 23:13:05 +0530

    [khushbu] adding create addressbook table

--- AddressBookServicesDB.sql
+++ AddressBookServicesDB.sql
@@ -1,4 +1,16 @@
 -- Welcome to addressbook services database
 
 -- UC1 Create addressbook services database 
-create database AddressBookServicesDB
+create database AddressBookServicesDB;
+use AddressBookServicesDB;
+-- UC2-
+CREATE TABLE  Customer(Customer_Id INT AUTO_INCREMENT PRIMARY KEY,
+FirstName varchar(225), 
+LastName varchar(225),
+Address varchar(225),
+City varchar(50),
+State varchar(50),
+Zip varchar (10),
+PhoneNumber bigint (10),
+Email varchar(50));
+SELECT * FROM Customer;
